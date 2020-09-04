9810aeb5a1c2cb409f17fe60facfe341e5281dcd
Th3Wall <mandellidavide.95@gmail.com>
2020-09-04 23:03:20 +0200

    Added SASS support; Fixed UI; Added some little data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import { fetchWeather } from './api/fetchWeather.js';
 import SearchIcon from '@material-ui/icons/Search';
-import './App.css';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import './App.sass';
 
 
 const App = () => {
@@ -10,7 +12,7 @@ const App = () => {
   const [weather, setWeather] = useState({});
   
   const search = async (e) => {
-    if(e.key === 'Enter') {
+    if(e.key === 'Enter' && query) {
       const data = await fetchWeather(query);
 
       setWeather(data);
@@ -23,25 +25,49 @@ const App = () => {
 
       <h1>Weather PWA</h1>
       <h5>Courtesy of OpenWeather API</h5>
+
       <div className="search-wrp">
         <SearchIcon />
-        <input type="text" className="search" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
+        <input type="text" className="search" placeholder="Search any city you want..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
       </div>
+
       {weather.main && (
-          <div className="city">
-              <h2 className="city-name">
-                  <span>{weather.name}</span>
-              </h2>
-              <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-              <div className="city-temp">
-                {Math.round(weather.main.temp)}
-                <sup>&deg;C</sup>
-              </div>
-              <div className="city-minmax">
-                <smaller>{Math.round(weather.main.temp_min)}<sup>&deg;</sup> / {Math.round(weather.main.temp_max)}<sup>&deg;</sup></smaller>
-              </div>
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup className="city-name-sup">{weather.sys.country}</sup>
+          </h2>
+          <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
           </div>
+          <div className="city-desc">
+            <h5>{weather.weather[0].description}</h5>
+          </div>
+          <div className="city-minmax">
+            <div className="min">
+              <div className="city-minmax-icon">
+                <ArrowDownwardIcon />
+                <smaller>{Math.round(weather.main.temp_min)}<sup>&deg;</sup></smaller>
+              </div>
+              <div className="city-minmax-label">
+                <p>Min.</p>
+              </div>
+            </div>
+            <div className="max">
+              <div className="city-minmax-icon">
+                <ArrowUpwardIcon />
+                <smaller>{Math.round(weather.main.temp_max)}<sup>&deg;</sup></smaller>
+              </div>
+              <div className="city-minmax-label">
+                <p>Max.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
+
     </div>
   );
 }
